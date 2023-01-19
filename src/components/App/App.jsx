404210bf0dab7bd2ede2e18bd64e2f95f03e56b0
Karl Kraemer <karlekraemer@gmail.com>
2023-01-19 11:26:30 -0600

    same commit with correct add .

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,13 +1,49 @@
+import axios from 'axios';
 import React from 'react';
+// import { useSelector} from 'react-redux';
+import { useState, useEffect } from 'react';
 import './App.css';
 
+// import our components
+import GalleryItem from '../Gallery/GalleryItem.jsx';
+import GalleryList from '../Gallery/GalleryList.jsx';
+
 function App() {
+
+  // will need a useSelector here maybe? Available for import above.
+
+  // get-ers and set-ers here
+  const [galleryList, setGalleryList] = useState([]);
+
+  // GET request
+  const getGallery = () => {
+    axios({
+      method: 'GET',
+      url: '/gallery'
+    })
+    .then((response) => {
+      console.log('response', response);
+      //array response log below, needed yet?
+      // console.log('just the data', response.data)
+      setGalleryList(response.data);
+    })
+    .catch(function (error) {
+      console.log('GET error', error);
+    });
+  }
+
+  // GET request on page load 
+  useEffect( () => {
+    getGallery();
+  }, [])
+
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Gallery of My Life</h1>
         </header>
-        <p>Gallery goes here</p>
+        {/* <p>Gallery goes here</p> */}
+        <GalleryList galleryListProp={galleryList} />
         <img src="images/goat_small.jpg"/>
       </div>
     );
---
+++ src/components/Gallery/GalleryList.jsx
@@ -0,0 +1,18 @@
+function GalleryList({galleryListProp}) {
+
+    return (
+        <>
+            {/* {JSON.stringify(props.creatureListProp)} */}
+            {/* <h2>HEADY HEADER</h2> */}
+            <ul>
+                {galleryListProp.map(item => 
+                    (<li key={item.id}>
+                        {item.path} {item.description} {item.likes}
+                    </li>)
+                )}
+            </ul>
+        </>
+    )
+}
+
+export default GalleryList;
