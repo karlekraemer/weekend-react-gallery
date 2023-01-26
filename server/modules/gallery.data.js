7aee4fc942e5cff9a32b8fdef2cae7a1e0ed2d67
Karl Kraemer <karlekraemer@gmail.com>
2023-01-26 10:01:17 -0600

    added an actual photo gallery and borrowed some CSS styling from John. Still need a like counter

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,6 +1,9 @@
 const galleryItems = [
     { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
-    { id: 2, path: 'images/test.png', description: 'test from random screenshot', likes: 0}
+    { id: 2, path: 'images/cat_in_a_bag.jpg', description: 'Wall-E staying cozy', likes: 0},
+    { id: 3, path: 'images/longs_peak.jpg', description: 'Tessa conquers Longs Peak for the first time!', likes: 0},
+    { id: 4, path: 'images/shawnee_national_forest.jpg', description: 'Adam and I after our weekend in Shawnee National Forest, Illinois', likes: 0},
+    { id: 5, path: 'images/medicine_bow.jpg', description: 'Elliott and I after Medicine Bow, Wyoming.', likes: 0}
 ];
 
 module.exports = galleryItems;
